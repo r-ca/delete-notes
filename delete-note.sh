faab8548ee3fc33bf62e499118f814e7c2139b88
ろむねこ <nem@nem.x0.to>
2022-09-02 17:26:35 +0900

    limit指定機能

--- delete-note.sh
+++ delete-note.sh
@@ -17,12 +17,25 @@ TOKEN=${TEMP:6}
 #TEMP='' #Initializing variables. #リプライ保護(todo)
 #TEMP=`cat ./config.txt | grep -s protectReplies`
 #REPLY_PROTECT=${TEMP:15}
+TEMP='' #Initializing variables. 
+TEMP=`cat ./config.txt | grep -s limit`
+LIMIT=${TEMP:6}
+
+#最大取得数検証
+if [ $LIMIT -le 1 ]; then
+    echo 'ERROR: Illegal limit value. (1~100)'
+    exit
+fi
+if [ $LIMIT -ge 100 ]; then
+    echo 'ERROR: Illegal limit value. (1~100)'
+    exit
+fi
 
 
 
 #ノートIDなど（検討中）
 OUTPUT='' #Initializing variables
-OUTPUT=`curl -X POST -s -H "Content-Type: application/json" -d '{"userId": "'$USERID'","i": "'$TOKEN'","limit": 100}' https://miss.nem.one/api/users/notes`
+OUTPUT=`curl -X POST -s -H "Content-Type: application/json" -d '{"userId": "'$USERID'","i": "'$TOKEN'","limit": '$LIMIT'}' https://miss.nem.one/api/users/notes`
 CREATED_AT=(`echo $OUTPUT | jq -r '.[] | .createdAt'`)
 NOTE_ID=(`echo $OUTPUT | jq -r '.[] | .id'`)
 
@@ -45,7 +58,7 @@ do
     length=$(($length-1))
     UNTIL_ID=${NOTE_ID[$length]}
 
-    OUTPUT=`curl -s -X POST -H "Content-Type: application/json" -d '{"userId": "'$USERID'","i": "'$TOKEN'","untilId": "'$UNTIL_ID'","limit": 100}' https://miss.nem.one/api/users/notes`
+    OUTPUT=`curl -s -X POST -H "Content-Type: application/json" -d '{"userId": "'$USERID'","i": "'$TOKEN'","untilId": "'$UNTIL_ID'","limit": '$LIMIT'}' https://miss.nem.one/api/users/notes`
     CREATED_AT+=(`echo $OUTPUT | jq -r '.[] | .createdAt'`)
     NOTE_ID+=(`echo $OUTPUT | jq -r '.[] | .id'`)
 
@@ -76,8 +89,8 @@ CURRENT_TIME_UNIX=`date -u +%s`
 c=0
 for n in "${CREATED_AT[@]}"
 do
-    if [ $(($CURRENT_TIME_UNIX - $n)) -ge 3600 ]; then
-        sleep 1
+    if [ $(($CURRENT_TIME_UNIX - $n)) -ge 21600 ]; then
+        sleep 2
         curl -s -X POST -H "Content-Type: application/json" -d '{"noteId": "'${NOTE_ID[c]}'","i": "'$TOKEN'"}' https://miss.nem.one/api/notes/delete
     else
         echo Protected
